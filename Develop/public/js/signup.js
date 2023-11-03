ef52bf1030e5e7de5895a2fcf1e43957c21357a2
unknown <anthonyaadamson@gmail.com>
2023-11-02 20:13:54 -0400

    added a signup page and route.. not working

---
+++ Develop/public/js/signup.js
@@ -0,0 +1,28 @@
+const signupFormHandler = async (event) => {
+    // Stop the browser from submitting the form so we can do so with JavaScript
+    event.preventDefault();
+  
+    // Gather the data from the form elements on the page
+    const email = document.querySelector('#email-signup');
+    const password = document.querySelector('#password-signup');
+    const username = document.querySelector('#username-signup');
+  
+    if (email && password && username) {
+      // Send the e-mail and password to the server
+      const response = await fetch('/api/users/signup', {
+        method: 'POST',
+        body: JSON.stringify({ username, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
