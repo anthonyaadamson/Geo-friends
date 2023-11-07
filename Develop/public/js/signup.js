6794c635e13646eb8f75e4451bc11b65c8156ac2
unknown <anthonyaadamson@gmail.com>
2023-11-06 19:02:37 -0500

    added map, not showing up

--- Develop/public/js/signup.js
+++ Develop/public/js/signup.js
@@ -18,7 +18,7 @@ const signupFormHandler = async (event) => {
       if (response.ok) {
         document.location.replace('/');
       } else {
-        alert('Failed to log in');
+        alert('Failed to signup in');
       }
     }
   };
